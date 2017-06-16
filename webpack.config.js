c1c1ee2805f40b74c147392390a993ffc1b49e67
KB <zhou9402@icloud.com>
2017-06-16 19:03:01 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const webpack = require('webpack');
+const path = require('path');
+
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
